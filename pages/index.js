28f019d9aeabaaa70bcf5019842e4d8d61030b51
Micah R Ledbetter <me@micahrl.com>
2020-05-24 18:08:45 -0500

    Add all keys in base layer
    * Use fixed sized keys
    * Fix React error about missing `key` property from items in list
    * Make layout look nicer

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Keyboard } from "../components/keyboard";
 export default function Home() {
   return (
     <div className="text-sm md:text-base p-4">
-      <div style={{ maxWidth: 500 }} className="my-24 container mx-auto">
+      <div style={{ maxWidth: 750 }} className="my-24 container mx-auto">
         <Head>
           <title>ErgoDox-EZ Keyboard Layout</title>
           <link rel="icon" href="/favicon.ico" />
