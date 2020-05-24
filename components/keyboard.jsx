a4fbf22c40d80164b21c849782ca92863caacf1c
Micah R Ledbetter <me@micahrl.com>
2020-05-24 14:32:53 -0500

    Break code into methods to prep for more board segments

--- components/keyboard.jsx
+++ components/keyboard.jsx
@@ -1,15 +1,16 @@
 import { useState, useRef, useEffect } from "react";
 import classnames from "classnames";
-import { keys } from "../lib/keys";
+import { leftHandKeys } from "../lib/keys";
 
 export const Keyboard = () => {
   const [pressedKey, setPressedKey] = useState({});
 
+  const allKeys = leftHandKeys
+
   const handleKeyDown = e => {
     e.keyCode === 9 && e.preventDefault();
-    const found = keys.find(key => key.keyCode === e.keyCode);
+    const found = allKeys.find(key => key.keyCode === e.keyCode);
     if (!found) return;
-
     setPressedKey(found);
   };
 
@@ -21,45 +22,53 @@ export const Keyboard = () => {
   const infoText = () => {
     if (pressedKey.key && pressedKey.info) {
       return `${pressedKey.key}: ${pressedKey.info}`
-    } else if (!pressedKey.info) {
-        return "Nothing interesting to say. (Don't pick an alphanumeric key.)"
     } else {
       return "Select a key"
     }
   }
 
+  const keyClassNames = (col, row, colStart, rowStart) => {
+    return classnames(
+      `col-span-${col} row-span-${row} col-start-${colStart} row-start-${rowStart} hover:bg-gray-400 cursor-pointer p-1 flex justify-center items-center rounded-sm`,
+      {
+        "bg-gray-400 border border-blue-500 shadow-outline": false, /* TODO: should be true when this key is selected */
+        "bg-gray-200 border border-gray-500 focus:outline-none": true /* TODO: should be true when this key is not selected */
+      }
+    )
+  }
+
+  const keyButtons = (keybKey) => {
+    const {
+      key,
+      size=[2, 2],
+      startPos=['auto', 'auto'],
+    } = keybKey
+    const [col, row] = size
+    const [colStart, rowStart] = startPos
+    return key !== null ? (
+      <button
+        onClick={() => setPressedKey(keybKey)}
+        className={keyClassNames(col, row, colStart, rowStart)}
+      >{key}</button>
+    ) : (
+      <div
+        className={`col-span-${col} row-span-${row} col-start-auto row-start-auto`}
+      />
+    )
+  }
+
+  const leftHandBoard = () => {
+    return (
+      <div className="grid grid-cols-15 grid-rows-10 ">
+        {leftHandKeys.map(keyButtons)}
+      </div>
+    )
+  }
+
   return (
     <div>
       <div className="pb-12">{infoText()}</div>
-      <div className="grid grid-cols-15 grid-rows-10 ">
-        {keys.map((keybKey, idx) => {
-          const {
-            key,
-            size=[2, 2],
-            startPos=['auto', 'auto'],
-          } = keybKey
-          const [col, row] = size
-          const [colStart, rowStart] = startPos
-          return key !== null ? (
-            <button
-              key={idx}
-              onClick={() => setPressedKey(keybKey)}
-              className={classnames(
-                `col-span-${col} row-span-${row} col-start-${colStart} row-start-${rowStart} hover:bg-gray-400 cursor-pointer px-1 py-2 md:p-4 flex justify-center items-center rounded-sm`,
-                {
-                  "bg-gray-400 border border-blue-500 shadow-outline": false, /* TODO: should be true when this key is selected */
-                  "bg-gray-200 border border-gray-500 focus:outline-none": true /* TODO: should be true when this key is not selected */
-                }
-              )}
-            >{key}</button>
-          ) : (
-            <div
-              key={idx}
-              className={`col-span-${col} row-span-${row} col-start-auto row-start-auto`}
-            />
-          )
-        })}
-      </div>
+      {leftHandBoard()}
     </div>
   )
 }
