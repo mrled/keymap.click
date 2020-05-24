90bdab255567f53ba848190bcc5de35f989c4ba5
Micah R Ledbetter <me@micahrl.com>
2020-05-24 18:08:45 -0500

    Add all keys in base layer
    * Use fixed sized keys
    * Fix React error about missing `key` property from items in list
    * Make layout look nicer

--- components/keyboard.jsx
+++ components/keyboard.jsx
@@ -1,11 +1,14 @@
 import { useState, useRef, useEffect } from "react";
 import classnames from "classnames";
-import { leftHandKeys } from "../lib/keys";
+import { leftHandKeys, leftThumbKeys, rightHandKeys, rightThumbKeys } from "../lib/keys";
 
 export const Keyboard = () => {
   const [pressedKey, setPressedKey] = useState({});
 
-  const allKeys = leftHandKeys
+  const allKeys = leftHandKeys.slice(0).concat(leftThumbKeys, rightHandKeys, rightThumbKeys)
+  allKeys.forEach((key, idx) => {
+    key.idx = idx
+  })
 
   const handleKeyDown = e => {
     e.keyCode === 9 && e.preventDefault();
@@ -20,16 +23,19 @@ export const Keyboard = () => {
   }, []);
 
   const infoText = () => {
-    if (pressedKey.key && pressedKey.info) {
-      return `${pressedKey.key}: ${pressedKey.info}`
-    } else {
-      return "Select a key"
-    }
+    return (<>
+      <h2 className="text-2xl">Key information</h2>
+      {pressedKey.info ? (
+        `${pressedKey.key}: ${pressedKey.info}`
+      ) : (
+        "Select a key"
+      )}
+    </>)
   }
 
   const keyClassNames = (col, row, colStart, rowStart) => {
     return classnames(
-      `col-span-${col} row-span-${row} col-start-${colStart} row-start-${rowStart} hover:bg-gray-400 cursor-pointer p-1 flex justify-center items-center rounded-sm`,
+      `col-span-${col} row-span-${row} col-start-${colStart} row-start-${rowStart} hover:bg-gray-400 cursor-pointer p-1 flex justify-center items-center rounded-sm text-xs`,
       {
         "bg-gray-400 border border-blue-500 shadow-outline": false, /* TODO: should be true when this key is selected */
         "bg-gray-200 border border-gray-500 focus:outline-none": true /* TODO: should be true when this key is not selected */
@@ -39,6 +45,7 @@ export const Keyboard = () => {
 
   const keyButtons = (keybKey) => {
     const {
+      idx,
       key,
       size=[2, 2],
       startPos=['auto', 'auto'],
@@ -47,28 +54,68 @@ export const Keyboard = () => {
     const [colStart, rowStart] = startPos
     return key !== null ? (
       <button
+        key={idx}
         onClick={() => setPressedKey(keybKey)}
         className={keyClassNames(col, row, colStart, rowStart)}
       >{key}</button>
     ) : (
       <div
+        key={idx}
         className={`col-span-${col} row-span-${row} col-start-auto row-start-auto`}
       />
     )
   }
 
   const leftHandBoard = () => {
-    return (
-      <div className="grid grid-cols-15 grid-rows-10 ">
-        {leftHandKeys.map(keyButtons)}
+    return (<>
+      <div className="float-left">
+        <h2 className="text-2xl">Left hand board</h2>
+        <div className="grid grid-cols-15-keyb grid-rows-10-keyb p-5">
+          {leftHandKeys.map(keyButtons)}
+        </div>
       </div>
-    )
+    </>)
+  }
+
+  const leftThumbBoard = () => {
+    return (<>
+      <div className="float-right">
+        <h2 className="text-2xl">Left thumb cluster</h2>
+        <div className="grid grid-cols-6-keyb grid-rows-6-keyb p-5">
+          {leftThumbKeys.map(keyButtons)}
+        </div>
+      </div>
+    </>)
+  }
+  const rightHandBoard = () => {
+    return (<>
+      <div className="float-right">
+        <h2 className="text-2xl">Right hand board</h2>
+        <div className="grid grid-cols-15-keyb grid-rows-10-keyb p-5">
+          {rightHandKeys.map(keyButtons)}
+        </div>
+      </div>
+    </>)
+  }
+
+  const rightThumbBoard = () => {
+    return (<>
+      <div className="float-left">
+        <h2 className="text-2xl">Right thumb cluster</h2>
+        <div className="grid grid-cols-6-keyb grid-rows-6-keyb p-5">
+          {rightThumbKeys.map(keyButtons)}
+        </div>
+      </div>
+    </>)
   }
 
   return (
     <div>
       <div className="pb-12">{infoText()}</div>
       {leftHandBoard()}
+      {leftThumbBoard()}
+      {rightHandBoard()}
+      {rightThumbBoard()}
     </div>
   )
 }
