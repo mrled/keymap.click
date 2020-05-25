97048b9aeb6aec28236c84d40c0c94f6ac544abb
Micah R Ledbetter <me@micahrl.com>
2020-05-24 23:48:14 -0500

    Add intro text

--- components/keyboard.jsx
+++ components/keyboard.jsx
@@ -87,8 +87,34 @@ export const Keyboard = () => {
     </>)
   }
 
+  const introTextDiv = () => {
+    return (<>
+      <div className="pb-20">
+        <h1 className="text-2xl text-blue-600">keyblay: Experiments in keyboard layouts</h1>
+        <p className="p-1">This is a work in progress.</p>
+        <p className="p-1">
+          I am building this to show off keyboard layouts for my ErgoDox,
+          and provide explanations for why I made the layout decisions I made.
+          The ErgoDox-EZ has been a huge part of my strategy for dealing with RSI,
+          and I want to be able to visually explain to others how it helped me.
+        </p>
+        <p className="p-1">
+          I am building it on GitHub.
+          Issues and contributions welcome.
+          {" "}
+          <a
+            className="text-blue-600 underline"
+            href="https://github.com/mrled/keyblay">
+              https://github.com/mrled/keyblay
+          </a>
+        </p>
+      </div>
+    </>)
+  }
+
   return (<>
     <div className="flex flex-col h-full">
+      {introTextDiv()}
       {boardSection('left', 'Left hand board', 15, 10, leftHandKeys)}
       {boardSection('right', 'Left thumb cluster', 6, 6, leftThumbKeys)}
       {boardSection('right', 'Right hand board', 15, 10, rightHandKeys)}
