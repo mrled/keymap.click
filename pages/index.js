057eff6c2e60a1c10c530980528f53ebcabc4730
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2020-05-26 00:02:12 -0500

    update layout. moves info section to right side of screen on desktop

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ import { Keyboard } from "../components/keyboard";
 export default function Home() {
   return (
     <div className="text-sm md:text-base p-4">
-      <div style={{ maxWidth: 750 }} className="my-24 container mx-auto">
+      <div
+        style={{ maxWidth: 1024 }}
+        className="my-8 md:my-24 container mx-auto"
+      >
         <Head>
           <title>ErgoDox-EZ Keyboard Layout</title>
           <link rel="icon" href="/favicon.ico" />
@@ -12,5 +15,5 @@ export default function Home() {
         <Keyboard />
       </div>
     </div>
-  )
+  );
 }
