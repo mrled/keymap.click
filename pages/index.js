7fd970dcf4e293ae7d3f38888370782c1a5b8e52
Micah R Ledbetter <me@micahrl.com>
2020-06-01 00:35:25 -0500

    WIP: Lots of experimentation, defined next steps, needs refactor

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,14 @@ import Head from "next/head";
 import { Keyboard } from "../components/keyboard";
 export default function Home() {
   return (
-    <div className="text-sm md:text-base p-4">
-      <div
-        style={{ maxWidth: 1024 }}
-        className="my-8 md:my-24 container mx-auto"
-      >
-        <Head>
-          <title>ErgoDox-EZ Keyboard Layout</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Keyboard />
+    <>
+      <Head>
+        <title>ErgoDox-EZ Keyboard Layout</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Keyboard maxWidth="1024" />
       </div>
-    </div>
+    </>
   );
 }
