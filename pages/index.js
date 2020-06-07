101f894649f33cb2dfdab3b7c6f66cae32ad9884
Micah R Ledbetter <me@micahrl.com>
2020-06-06 22:12:04 -0500

    Simplify Keyboard component

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Keyboard maxWidth="1024" />
+        <Keyboard />
       </div>
     </>
   );
