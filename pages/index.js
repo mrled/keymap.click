0427ced08a2d5b87073ca79fd561f8afe3f464fd
Micah R Ledbetter <me@micahrl.com>
2020-06-14 14:52:09 -0500

    Add some og: stuff for twitter cards

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,43 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ErgoDox-EZ Keyboard Layout</title>
+        <title>keyblay: experiments in keyboard layouts</title>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+
+        <meta
+          name="og:title"
+          property="og:title"
+          content="keyblay: experiments in keyboard layouts"
+        />
+        <meta
+          name="og:description"
+          property="og:description"
+          content="A visual explanation of how an ErgoDox keyboard helped my RSI."
+        />
+        <meta property="og:url" content="https://keyblay.now.sh" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/command_16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/command_32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/command_128x128.png"
+          sizes="128x128"
+        />
+        <meta property="og:image" content="/og_image_screenshot.png" />
       </Head>
       <div>
         <Keyboard />
