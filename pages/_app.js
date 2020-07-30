bee43ad07412efcf8feda6905b6e6ad92fd3c5f5
Micah R Ledbetter <me@micahrl.com>
2020-07-29 22:40:40 -0500

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,24 @@ import "../styles/index.css";
 import "../styles/keygrid.css";
 import "../styles/fonts.css";
 import "../styles/debug.css";
+import * as Fathom from 'fathom-client'
+
 function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    Fathom.load('HDMUSVII', {
+      includedDomains: ['keyblay.now.sh']
+    });
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    }
+  }, [])
+
   return <Component {...pageProps} />;
 }
+
 export default App;
