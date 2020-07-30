0395d70c987edcaf69840abdec49e2c282cec0bd
Micah R Ledbetter <me@micahrl.com>
2020-07-29 22:33:18 -0500

    Show KeyInfo below the board again
    Use a small KeyGrid to hold the Key in the KeyInfo

--- components/keyInfo.jsx
+++ components/keyInfo.jsx
@@ -4,7 +4,7 @@ import {
   keyInfoConnectFromClass,
   keyInfoConnectFromClassPrefix,
 } from "../lib/keyConnections";
-import { Key } from "./key";
+import { keyDataTextLabel, Key, KeyGrid } from "./key";
 
 /* Parse a keyInfo property
   * Return an array of JSX <span> elements.
@@ -85,27 +85,50 @@ export const KeyInfoInner = ({ keyData, parsedKeyInfo }) => {
 };
 
 /* Return a KeyInfoInner component, wrapped in a <KeyInfo> component
- * styled nicely for the parent <Keyboard> component
+ * styled nicely for the parent <Keyboard> component.
+ * Include an entire <KeyGrid> just large enough to hold the largest key.
  */
 export const KeyInfo = ({
   keyData,
   parsedKeyInfo,
   keyButtonOnClick = () => { },
 }) => {
+  let modifiedKeyData = Object.assign({}, keyData);
+  modifiedKeyData.startPos = [0, 0];
+  const keys = [modifiedKeyData]
+  const textLabel = keyDataTextLabel(modifiedKeyData);
   return (
     <>
       <div className="border-b pb-2 mb-2">
-        <div className="flex justify-between">
-          <div>
-            <h2 className="text-2xl">Key information</h2>
-            <button onClick={keyButtonOnClick} className="block text-blue-500">
-              deselect key
-            </button>
+        <div className="flex justify-evenly">
+
+          <div className="flex flex-col flex-1">
+            <div className="flex flex-row justify-start">
+              <div className="flex flex-col">
+                <h2 className="text-sm md:text-2xl">Key information</h2>
+                <button onClick={keyButtonOnClick} className="block text-blue-500">
+                  deselect key
+              </button>
+              </div>
+            </div>
           </div>
-          <div>
-            <Key keyData={keyData} standalone={true} extraClasses="inline" />
-            <span className="p-5 font-mono">{keyData.name}</span>
+
+          <div className="flex flex-col flex-1">
+            <div className="flex flex-row justify-center">
+              <KeyGrid
+                cols="4"
+                rows="4"
+                keys={keys}
+              />
+            </div>
           </div>
+
+          <div className="flex flex-col flex-1">
+            <div className="flex flex-row justify-end">
+              <span className="p-5 font-mono">{textLabel}</span>
+            </div>
+          </div>
+
         </div>
       </div>
       <KeyInfoInner keyData={keyData} parsedKeyInfo={parsedKeyInfo} />
