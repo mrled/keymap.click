cfd4e96f102d2f950757f4b35f9cfff3ffbc33c7
Micah R Ledbetter <me@micahrl.com>
2024-03-06 19:59:16 -0600

    Remove special handling for string-constructed tailwind classes that we are no longer using

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   content: ["./src/**/*.js", "./src/**/*.jsx"],
-  safelist: [
-    // TODO: rename all these classes with some string constant
-    { pattern: /^row\-span.*/ },
-    { pattern: /^row\-start.*/ },
-    { pattern: /^col\-span.*/ },
-    { pattern: /^col\-start.*/ },
-  ],
 };
