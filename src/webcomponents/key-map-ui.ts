51870df034bd879e318bf960d8142f7ce9b597d9
Micah R Ledbetter <me@micahrl.com>
2024-03-29 22:37:35 -0500

    Refactor KeyMapUIOptions into its own file

--- src/webcomponents/key-map-ui.ts
+++ src/webcomponents/key-map-ui.ts
@@ -1,8 +1,9 @@
 import log from "loglevel";
 
+import { KeyMapUIOptions } from "~/lib/KeyMapUIOptions";
 import { drawDiagram } from "~/lib/diagram";
 import { ConnectionPair, KeyInfoConnectType } from "~/lib/keyConnections";
-import { KeyMap, KeyMapKey } from "~/lib/keyMap";
+import { KeyMap } from "~/lib/keyMap";
 
 import { KeyHandle } from "~/webcomponents/key-handle";
 import { KeyIndicator } from "~/webcomponents/key-indicator";
@@ -856,82 +857,5 @@ export class KeyMapUI extends HTMLElement {
       `${logPrefix}: current raw query string: ${window.location.search}, current state:`
     );
     console.table(logTable);
-  }
-}
-
-/* The options that can be passed in the query string.
- */
-class KeyMapUIOptions {
-  prefix: string;
-  board: string;
-  map: string;
-  layer: string;
-  id: string;
-
-  constructor({
-    prefix,
-    board,
-    map,
-    layer,
-    id,
-  }: {
-    prefix: string;
-    board?: string;
-    map?: string;
-    layer?: string;
-    id?: string;
-  }) {
-    this.prefix = prefix;
-    this.board = board || "";
-    this.map = map || "";
-    this.layer = layer || "";
-    this.id = id || "";
-  }
-
-  /* Parse a query string and return a QueryStringParams object.
-   *
-   * Can be passed the current window query string with window.location.search.
-   *
-   * Returns a QueryStringParams object and the input queryString with any parsed parameters removed.
-   */
-  static parseQueryString(
-    prefix: string,
-    queryString: string
-  ): [KeyMapUIOptions, URLSearchParams] {
-    const params = new URLSearchParams(queryString);
-    const newParams = new URLSearchParams();
-    const result = new KeyMapUIOptions({ prefix });
-    params.forEach((value, key) => {
-      switch (key) {
-        case `${prefix}-board`:
-          result.board = value;
-          break;
-        case `${prefix}-map`:
-          result.map = value;
-          break;
-        case `${prefix}-layer`:
-          result.layer = value;
-          break;
-        case `${prefix}-id`:
-          result.id = value;
-          break;
-        default:
-          newParams.set(key, value);
-          break;
-      }
-    });
-    return [result, newParams];
-  }
-
-  /* True if any of the parameters are set.
-   */
-  get any(): boolean {
-    return !!this.board || !!this.map || !!this.layer || !!this.id;
-  }
-
-  /* Show a nice string
-   */
-  toString(): string {
-    return `KeyMapUIOptions: prefix: ${this.prefix}, board: ${this.board}, map: ${this.map}, layer: ${this.layer}, id: ${this.id}`;
   }
 }
