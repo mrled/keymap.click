fea9d59aa5899356175bd3ab1d3fbd6573bfafa7
Micah R Ledbetter <me@micahrl.com>
2024-04-22 22:10:46 -0500

    Remove unused keymap.click stuff, improve styles
    Remove all legend images, the font, and other unused assets
    
    Use orange as the site accent color for links and favicon

--- sites/keymap.click/website.shared.js
+++ sites/keymap.click/website.shared.js
@@ -1,13 +1,3 @@
 // Shared JS for all pages on the website
 
-// Styles specific to the website - not part of the webcomponents
 import "./website.style.css";
-
-// // Ordered styles
-// import "./src/styles/vars.css";
-// import "./src/styles/root.css";
-// // Alphabetical styles
-// import "./src/styles/diagram.css";
-// // import "./src/styles/fonts.css";
-// import "./src/styles/keygrid.css";
-// import "./src/styles/keyInfoPanel.css";
