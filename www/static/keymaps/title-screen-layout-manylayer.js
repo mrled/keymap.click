8df8bb69dc94337a0d5e38b1e29ddca96ccb4b1e
Micah R Ledbetter <me@micahrl.com>
2025-06-20 07:34:47 -0500

    Add guides to the manylayer example layout

--- www/static/keymaps/title-screen-layout-manylayer.js
+++ www/static/keymaps/title-screen-layout-manylayer.js
@@ -1,5 +1,10 @@
 import { KeyboardModelTitleScreen } from "./title-screen-keyboard.js";
-import { KeymapLayout, KeymapKey, KeymapLayer } from "@keymap.click/ui";
+import {
+  KeymapGuide,
+  KeymapLayout,
+  KeymapKey,
+  KeymapLayer,
+} from "@keymap.click/ui";
 
 /* A dumb function to generate a layer, for testing a board with lots of layers
  */
@@ -26,6 +31,21 @@ function generateLayer(layerIdx) {
   });
 }
 
+const allKeySelection = [
+  "title-screen-1-1",
+  "title-screen-3-1",
+  "title-screen-5-1",
+  "title-screen-7-1",
+  "title-screen-9-1",
+  "title-screen-11-1",
+  "title-screen-13-1",
+  "title-screen-15-1",
+  "title-screen-17-1",
+  "title-screen-19-1",
+  "title-screen-21-1",
+  "title-screen-23-1",
+];
+
 /* A board with lots of layers, for testing purposes.
  */
 export const KeymapTitleScreenLayoutManyLayer = new KeymapLayout({
@@ -34,5 +54,66 @@ export const KeymapTitleScreenLayoutManyLayer = new KeymapLayout({
   model: KeyboardModelTitleScreen,
   // Generate 32 layers, which IIRC is QMK's max
   layers: Array.from({ length: 32 }, (_, i) => i).map(generateLayer),
-  guides: [],
+  guides: [
+    new KeymapGuide({
+      title: "First Guide",
+      shortName: "Guide1",
+      id: "guide1",
+      steps: [
+        {
+          title: "Welcome to the guide to my main keyboard layout",
+          text: [
+            `
+              This is the title board.
+              All the keys are highlighted in orange.
+            `,
+          ],
+          selection: allKeySelection,
+        },
+        {
+          title: "First key",
+          text: [
+            `
+            Now just one key is highlighted.
+            `,
+          ],
+          selection: "title-screen-1-1",
+        },
+        {
+          title: "The end",
+          text: [`Thanks for all the clicks.`],
+        },
+      ],
+    }),
+    new KeymapGuide({
+      title: "Second Guide",
+      shortName: "Guide2",
+      id: "guide2",
+      steps: [
+        {
+          title: "This is just a copy of the first guide",
+          text: [
+            `
+            We use this copy for testing purposes.
+            Here again all the keys are highlighted in orange.
+            `,
+          ],
+          selection: allKeySelection,
+        },
+        {
+          title: "First key",
+          text: [
+            `
+            Now just one key is highlighted.
+            `,
+          ],
+          selection: "title-screen-1-1",
+        },
+        {
+          title: "The end",
+          text: [`Thanks for all the clicks.`],
+        },
+      ],
+    }),
+  ],
 });
