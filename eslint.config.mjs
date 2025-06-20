ae892b4f45dd70e53500cbd381afbc0744e5b888
Micah R Ledbetter <me@micahrl.com>
2025-06-20 15:11:08 -0500

    Set up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,73 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [
+      "*/dist",
+      "**/node_modules",
+      "**/package-lock.json",
+      "**/.vscode",
+      "www/_site",
+      "www/static/keymap.click",
+    ],
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,mts,cts}"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    files: ["www/eleventy.*.js"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+    rules: {
+      "css/use-baseline": "off", // Sorry I need nesting
+      "css/no-invalid-properties": "off", // It doesn't understand a vars.css file that has variables used in other files
+    },
+  },
+]);
